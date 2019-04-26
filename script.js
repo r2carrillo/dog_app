c9037b6e82b7e34a0d801821cd0de1a04ec8c093
carlos arturo carrillo <r2carrillo@hotmail.com>
2019-04-25 20:28:19 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//alert('HELLO!');
+var firstname = prompt('¿CUAL ES TU PRIMER NOMBRE?');
+var lastname = prompt('¿CUAL ES TU SEGUNDO NOMBRE?');
+var age = prompt('¿CUAL ES TU EDAD?');
+var nombrecompleto = firstname + " " +  lastname;
+console.log( `TU NOMBRE ES : ${nombrecompleto}`);
+console.log( `TU EDAD ES : ${age} AÑOS`);
